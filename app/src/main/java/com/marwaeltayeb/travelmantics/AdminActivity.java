5c26c3cedb900148556c034868b21f39792509ae
Marwa-Said <marwa_eltayeb@yahoo.com>
2019-08-03 23:13:07 +0200

    Use FirebaseUtil class

--- app/src/main/java/com/marwaeltayeb/travelmantics/AdminActivity.java
+++ app/src/main/java/com/marwaeltayeb/travelmantics/AdminActivity.java
@@ -28,9 +28,10 @@ public class AdminActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mFirebaseDatabase = FirebaseDatabase.getInstance();
-        mDatabaseReference = mFirebaseDatabase.getReference().child("traveldeals");
+        
+        FirebaseUtil.openFbReference("traveldeals");
+        mFirebaseDatabase = FirebaseUtil.mFirebaseDatabase;
+        mDatabaseReference = FirebaseUtil.mDatabaseReference;
 
         txtTitle = (EditText) findViewById(R.id.txtTitle);
         txtPrice = (EditText) findViewById(R.id.txtPrice);
