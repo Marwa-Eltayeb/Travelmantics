2249ccec10aef3ac317f869fc00b5600d06d2910
Marwa-Said <marwa_eltayeb@yahoo.com>
2019-08-03 21:55:16 +0200

    Travelmantics

---
+++ app/src/main/java/com/marwaeltayeb/travelmantics/AdminActivity.java
@@ -0,0 +1,80 @@
+package com.marwaeltayeb.travelmantics;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class AdminActivity extends AppCompatActivity {
+
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mDatabaseReference;
+
+    EditText txtTitle;
+    EditText txtPrice;
+    EditText txtDescription;
+    Button btnSelectImage;
+    ImageView imageTravel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mDatabaseReference = mFirebaseDatabase.getReference().child("traveldeals");
+
+        txtTitle = (EditText) findViewById(R.id.txtTitle);
+        txtPrice = (EditText) findViewById(R.id.txtPrice);
+        txtDescription = (EditText) findViewById(R.id.txtDescription);
+        btnSelectImage = (Button) findViewById(R.id.btnSelectImage);
+        imageTravel = (ImageView) findViewById(R.id.imageTravel);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.save_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.save_menu:
+                saveDeal();
+                Toast.makeText(this, "Deal saved", Toast.LENGTH_LONG).show();
+                clean();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void saveDeal() {
+        String title = txtTitle.getText().toString();
+        String price = txtPrice.getText().toString();
+        String description = txtDescription.getText().toString();
+
+        TravelDeal deal = new TravelDeal(title,price,description,"");
+        mDatabaseReference.push().setValue(deal);
+
+    }
+
+    private void clean() {
+        txtTitle.setText("");
+        txtPrice.setText("");
+        txtDescription.setText("");
+        txtTitle.requestFocus();
+    }
+}
