9a6263dae45eec58925edc94d7f9b9474df603fb
Marwa-Said <marwa_eltayeb@yahoo.com>
2019-08-05 00:56:15 +0200

    Add authorization for admin

--- app/src/main/java/com/marwaeltayeb/travelmantics/FirebaseUtil.java
+++ app/src/main/java/com/marwaeltayeb/travelmantics/FirebaseUtil.java
@@ -1,22 +1,54 @@
 package com.marwaeltayeb.travelmantics;
 
+import android.support.annotation.NonNull;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
 public class FirebaseUtil {
+
     public static FirebaseDatabase mFirebaseDatabase;
     public static DatabaseReference mDatabaseReference;
     private static FirebaseUtil firebaseUtil;
+    public static FirebaseAuth mFirebaseAuth;
+    public static FirebaseAuth.AuthStateListener mAuthListener;
     public static ArrayList<TravelDeal> mDeals;
+    private static final int RC_SIGN_IN = 123;
+    private static UserActivity caller;
+    public static boolean isAdmin;
 
-    private FirebaseUtil(){};
+    private FirebaseUtil() {
+    }
 
-    public static void openFbReference(String ref) {
+    public static void openFbReference(String ref, final UserActivity  callerActivity) {
         if (firebaseUtil == null) {
             firebaseUtil = new FirebaseUtil();
             mFirebaseDatabase = FirebaseDatabase.getInstance();
+            mFirebaseAuth = FirebaseAuth.getInstance();
+
+            caller = callerActivity;
+
+            mAuthListener = new FirebaseAuth.AuthStateListener() {
+                @Override
+                public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                    if (firebaseAuth.getCurrentUser() == null) {
+                        FirebaseUtil.signIn();
+                    }
+                    else {
+                        String userId = firebaseAuth.getCurrentUser().getUid();
+                        checkAdmin(userId);
+                    }
+                    Toast.makeText(callerActivity.getBaseContext(), "Welcome back!", Toast.LENGTH_LONG).show();
+                }
+            };
 
         }
         // Reset the arrayList
@@ -24,4 +56,59 @@ public class FirebaseUtil {
         mDatabaseReference = mFirebaseDatabase.getReference().child(ref);
     }
 
+    private static void signIn() {
+        caller.startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setIsSmartLockEnabled(false)
+                        // Choose authentication providers
+                        .setProviders(
+                                AuthUI.EMAIL_PROVIDER,
+                                AuthUI.GOOGLE_PROVIDER)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    private static void checkAdmin(String uid) {
+        FirebaseUtil.isAdmin=false;
+        DatabaseReference ref = mFirebaseDatabase.getReference().child("administrators")
+                .child(uid);
+        ChildEventListener listener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                FirebaseUtil.isAdmin=true;
+                caller.showMenu();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+        ref.addChildEventListener(listener);
+    }
+
+
+    public static void attachListener() {
+        mFirebaseAuth.addAuthStateListener(mAuthListener);
+    }
+
+    public static void detachListener() {
+        mFirebaseAuth.removeAuthStateListener(mAuthListener);
+    }
 }
