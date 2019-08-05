ff09a3277e86093edbf3367f82338909daf4c4f0
Marwa-Said <marwa_eltayeb@yahoo.com>
2019-08-05 17:25:04 +0200

    Add imageName attribute

--- app/src/main/java/com/marwaeltayeb/travelmantics/TravelDeal.java
+++ app/src/main/java/com/marwaeltayeb/travelmantics/TravelDeal.java
@@ -9,15 +9,17 @@ public class TravelDeal implements Serializable{
     private String description;
     private String price;
     private String imageUrl;
+    private String imageName;
 
     public TravelDeal(){}
 
-    public TravelDeal(String title,String price ,String description, String imageUrl) {
+    public TravelDeal(String title,String price ,String description, String imageUrl, String imageName) {
         this.setId(id);
         this.setTitle(title);
         this.setPrice(price);
         this.setDescription(description);
         this.setImageUrl(imageUrl);
+        this.setImageName(imageName);
     }
 
     public String getId() {
@@ -58,5 +60,13 @@ public class TravelDeal implements Serializable{
 
     public void setImageUrl(String imageUrl) {
         this.imageUrl = imageUrl;
+    }
+
+    public String getImageName() {
+        return imageName;
+    }
+
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
     }
 }
